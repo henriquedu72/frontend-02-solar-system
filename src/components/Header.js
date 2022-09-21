40a857a585645910e449c28f33f99bf5ee1f1aee
eduardo <duh_henrique@hotmail.com>
2022-09-21 14:54:35 -0300

    requisito 1

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import { React } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <Header>
+        <h1>Sistema Solar</h1>
+      </Header>
+    );
+  }
+}
+
+export default Header;
